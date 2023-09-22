eee7124212a0c50a056c51a0e4475ec0ccc4148b
Muhammad Usama <imusamairshad@gmail.com>
2023-09-22 16:12:58 +0500

    groceries.dart is added.

---
+++ lib/data/groceries.dart
@@ -0,0 +1,44 @@
+class Groceries {
+  static List<Map<String, dynamic>> groceries = [
+    {
+      'id': '1',
+      'name': 'Apples',
+      'description': 'Fresh and juicy apples.',
+      'price': 1.99,
+      'imageUrl':
+          'https://post.healthline.com/wp-content/uploads/2020/09/Do_Apples_Affect_Diabetes_and_Blood_Sugar_Levels-732x549-thumbnail-1-732x549.jpg',
+    },
+    {
+      'id': '2',
+      'name': 'Bananas',
+      'description': 'Ripe and delicious bananas.',
+      'price': 0.99,
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/2324px-Banana-Single.jpg',
+    },
+    {
+      'id': '3',
+      'name': 'Bread',
+      'description': 'Freshly baked bread.',
+      'price': 2.49,
+      'imageUrl':
+          'https://assets.bonappetit.com/photos/5c62e4a3e81bbf522a9579ce/16:9/w_4000,h_2250,c_limit/milk-bread.jpg',
+    },
+    {
+      'id': '4',
+      'name': 'Milk',
+      'description': 'Pure and nutritious milk.',
+      'price': 1.79,
+      'imageUrl':
+          'https://images.healthshots.com/healthshots/en/uploads/2023/01/06161102/skimmed-milk-1600x900.jpg',
+    },
+    {
+      'id': '5',
+      'name': 'Eggs',
+      'description': 'Farm-fresh eggs.',
+      'price': 2.99,
+      'imageUrl':
+          'https://post.healthline.com/wp-content/uploads/2020/05/eggs-counter-1200x628-facebook-1200x628.jpg',
+    },
+  ];
+}
