b7e2b0b505895e3d8d32222faf8962fbd4354a6a
Muhammad Usama <imusamairshad@gmail.com>
2023-09-22 16:15:51 +0500

    home_state.dart is added.

---
+++ lib/features/home/bloc/home_state.dart
@@ -0,0 +1,21 @@
+part of 'home_bloc.dart';
+
+@immutable
+abstract class HomeState {}
+
+abstract class HomeActionState extends HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeLoadedSuccessState extends HomeState {
+  final List<ProductDataModel> products;
+  HomeLoadedSuccessState({required this.products});
+}
+
+class HomeErrorState extends HomeState {}
+
+class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+class HomeNavigateToCartPageActionState extends HomeActionState {}
