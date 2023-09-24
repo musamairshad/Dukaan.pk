172102baa549d37b002be96fdab49b16cce13bd0
Muhammad Usama <imusamairshad@gmail.com>
2023-09-24 19:37:07 +0500

    cart_tile_widget.dart is added.

---
+++ lib/features/cart/screens/cart_tile_widget.dart
@@ -0,0 +1,80 @@
+import 'package:bloc_learning/features/cart/bloc/cart_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:bloc_learning/features/home/models/home_product_data_model.dart';
+import 'package:bloc_learning/features/home/bloc/home_bloc.dart';
+
+class CartTileWidget extends StatelessWidget {
+  final ProductDataModel product;
+  final CartBloc cartBloc;
+  const CartTileWidget(
+      {super.key, required this.product, required this.cartBloc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.maxFinite,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            product.name,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(product.description),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\$ ${product.price}',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // homeBloc.add(HomeProductWishlistButtonClickedEvent(
+                      //   clickedProduct: product,
+                      // ));
+                    },
+                    icon: const Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      cartBloc.add(
+                        CartRemoveFromCartEvent(
+                          product: product,
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.shopping_bag),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
