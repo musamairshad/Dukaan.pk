e4fe7b431a2a884301b2b8eece707071fc3367cb
Muhammad Usama <imusamairshad@gmail.com>
2023-09-25 16:51:31 +0500

    wishlist_bloc.dart is added.

---
+++ lib/features/wishlist/bloc/wishlist_bloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:bloc_learning/data/wishlist_items.dart';
+import 'package:bloc_learning/features/home/models/home_product_data_model.dart';
+import 'package:meta/meta.dart';
+
+part 'wishlist_event.dart';
+part 'wishlist_state.dart';
+
+class WishlistBloc extends Bloc<WishlistEvent, WishlistState> {
+  WishlistBloc() : super(WishlistInitial()) {
+    on<WishlistInitialEvent>(wishlistInitialEvent);
+    on<WishlistRemoveFromWishlistEvent>(wishlistRemoveFromWishlistEvent);
+  }
+
+  FutureOr<void> wishlistInitialEvent(
+      WishlistInitialEvent event, Emitter<WishlistState> emit) {
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
+  }
+
+  FutureOr<void> wishlistRemoveFromWishlistEvent(
+      WishlistRemoveFromWishlistEvent event, Emitter<WishlistState> emit) {
+    wishlistItems.remove(event.product);
+    emit(WishlistSuccessState(wishlistItems: wishlistItems));
+    emit(HomeProductItemRemoveFromWishlistActionState());
+  }
+}
