e0d48433b5462dfb4dcc44a1255ad07d6a0ce9ae
Muhammad Usama <imusamairshad@gmail.com>
2023-09-25 16:52:22 +0500

    wishlist_state.dart is added.

---
+++ lib/features/wishlist/bloc/wishlist_state.dart
@@ -0,0 +1,17 @@
+part of 'wishlist_bloc.dart';
+
+@immutable
+abstract class WishlistState {}
+
+abstract class WishlistActionState extends WishlistState {}
+
+class WishlistInitial extends WishlistState {}
+
+class WishlistSuccessState extends WishlistState {
+  final List<ProductDataModel> wishlistItems;
+
+  WishlistSuccessState({required this.wishlistItems});
+}
+
+class HomeProductItemRemoveFromWishlistActionState
+    extends WishlistActionState {}
